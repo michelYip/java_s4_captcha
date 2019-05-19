6d74a1df9a3198ebd2636b93591a182ea6a04b95
Draelnir <lollier.guillaume@gmail.com>
2019-05-19 13:44:46 +0200

    import

--- src/fr/upem/captcha/captchamanager/CaptchaManager.java
+++ src/fr/upem/captcha/captchamanager/CaptchaManager.java
@@ -1,5 +1,10 @@
 package fr.upem.captcha.captchamanager;
 
+import fr.upem.captcha.images.Category;
+import fr.upem.captcha.images.animaux.Animaux;
+import fr.upem.captcha.images.boissons.Boissons;
+import fr.upem.captcha.images.legumes.Legumes;
+
 public class CaptchaManager {
 
 }
